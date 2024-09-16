133adced626557ff356ebbcad949766ae6b05a01
UladzislauSachyuka <94551428+UladzislauSachyuka@users.noreply.github.com>
2024-09-16 12:43:44 +0300

    the local time is displayed now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getForecast } from './api/weatherApi';
+import SunIcon from './assets/SVG/sun';
+import CloudyDayIcon from './assets/SVG/cloudyDay';
 import './styles/App.css'
 
 function App() {
@@ -7,6 +9,9 @@ function App() {
   const [forecast, setForecast] = useState(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
 
   useEffect(() => {
     const fetchForecast = async () => {
@@ -16,6 +21,11 @@ function App() {
         const data = await getForecast(location);
         setForecast(data);
         setLoading(false);
+
+        const now = new Date();
+        const localTime = getLocalTime(now, data.tzoffset);
+        setCurrentDate(formatDate(localTime));
+        setCurrentTime(formatTime(localTime));
       } catch (error) {
         setError(true);
         setForecast(null);
@@ -26,6 +36,57 @@ function App() {
     fetchForecast();
   }, [location]);
 
+  const getLocalTime = (date, tzoffset) => {
+    const utcTime = date.getTime() + date.getTimezoneOffset() * 60000; // Преобразуем в UTC
+    const localTime = new Date(utcTime + tzoffset * 3600000); // Добавляем смещение часового пояса
+    return localTime;
+  };
+
+  const handleSearch = () => {
+    if (inputValue) {
+      setLocation(inputValue);
+    }
+  };
+
+  const formatDate = (date) => {
+    const day = date.getDate();
+    const daySuffix = (day) => {
+      if (day === 1 || day === 21 || day === 31) return 'st';
+      if (day === 2 || day === 22) return 'nd';
+      if (day === 3 || day === 23) return 'rd';
+      return 'th';
+    };
+
+    const options = { weekday: 'long', month: 'short', day: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-GB', options);
+
+    const year = date.getFullYear();
+    const shortYear = year.toString().slice(-2);
+
+    return `${formattedDate.split(" ")[0]}, ${day}${daySuffix(day)} ${date.toLocaleString('en-GB', { month: 'short' })} '${shortYear}`;
+  };
+
+  const formatTime = (date) => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const suffix = hours >= 12 ? 'pm' : 'am';
+    const formattedTime = `${hours % 12 || 12}:${minutes < 10 ? '0' + minutes : minutes} ${suffix}`;
+    return formattedTime;
+  };
+
+  const formatTemp = (temp) => {
+    return Math.round(temp);
+  };
+
+  const getWeatherIcon = (condition) => {
+    switch (condition) {
+      case "Clear":
+        return <SunIcon className="weatherIcon"/>;
+      case "Partially cloudy":
+        return <CloudyDayIcon className="weatherIcon"/>;
+    }
+  };
+
   return (
     <div className="app">
       {loading ? (
@@ -35,14 +96,23 @@ function App() {
       ) : (
         forecast && (
           <div className="leftBlock">
-            <text className="conditionsText">{forecast["days"][0]["conditions"]}</text>
+            <text className="conditionsText">{forecast["currentConditions"]["conditions"]}</text>
+            <text className="cityText">{forecast["resolvedAddress"].split(',')[0]}</text>
+            <text className="dateText">{currentDate}</text>
+            <text className="timeText">{currentTime}</text>
+            <text className="tempText">{formatTemp(forecast["currentConditions"]["temp"]) + " °C"}</text>
+            <div className="weatherIcon"> 
+              {getWeatherIcon(forecast["currentConditions"]["conditions"])}
+            </div>
             <div className="searchBox">
               <input
-                type='text'
-                className='searchBoxInput'
-                placeholder='Search Location...'
+                type="text"
+                className="searchBoxInput"
+                placeholder="Search Location..."
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
               />
-              <div className='searchIcon'></div>
+              <div className="searchIcon" onClick={handleSearch}></div>
             </div>
           </div>
         )
--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -2,6 +2,7 @@ export const getForecast = async ( city ) => {
   try {
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=7ZPGW99A4CLSNTPHQTF7XYGEA&contentType=json`);
     const data = await response.json();
+    console.log(data);
     return data;
   } catch (error) {
     throw new Error(error);
